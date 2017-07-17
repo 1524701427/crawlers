cdd2d9a91bf7c5a7c4251530235dc8d6a4b521d3
wanglanwei <wanglanwei@weiche.cn>
2017-07-17 16:34:22 +0800

    add ProxyPool fetch.

--- proxy.py
+++ proxy.py
@@ -2,7 +2,14 @@
 
 from __future__ import with_statement, print_function
 
+from random import choice
 from collections import deque
+
+
+class ProxyUnavaliableError(StandardError):
+    '''
+    代理不可用。'''
+    pass
 
 
 class ProxyPoolEmptyError(StandardError):
@@ -18,7 +25,7 @@ class ProxyPool(object):
         初始化代理池。
 
         Args:
-            max_size (int): 指定代理池容量。
+            max_size (int): 指定代理池容量, None - 不限容量。
 
         Returns:
             None
@@ -26,6 +33,18 @@ class ProxyPool(object):
         self._max_size = max_size
         self._pool = deque(maxlen=max_size)
 
-    def fetch(self):
+    def fetch(self, random=True):
         '''
-        获取代理。'''
+        从代理池中获取一个代理。
+
+        Args:
+            random (bool): 是否随机获取。
+        '''
+        try:
+            if random is True:
+                proxy = choice(self._pool)
+            else:
+                proxy = self._pool.pop_left()
+            return proxy
+        except IndexError:
+            raise ProxyPoolEmptyError()
