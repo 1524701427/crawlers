57ece171cf102d482e0b621a5dc45091f544cce9
wanglanwei <wanglanwei@weiche.cn>
2017-07-21 17:25:43 +0800

    add new module.

---
+++ util/decorators.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+
+'''
+常用装饰器。'''
+
+import time
+import functools
+
+
+def timeit(func):
+    @functools.wraps(func)
+    def wrapper(*args, **kwargs):
+        t0 = time.time()
+        res = func(*args, **kwargs)
+        t1 = time.time()
+        print(">>> total time: " % round(t1 - t0, 2))
+        return res
+    return wrapper
